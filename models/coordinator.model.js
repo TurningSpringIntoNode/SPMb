44339501738ee4e6b7332ed46c087e93c227e18a
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2018-10-10 22:34:32 -0300

    Removing unnecessary methods

--- models/coordinator.model.js
+++ models/coordinator.model.js
@@ -1,76 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const jwt = require('jsonwebtoken');
-const keys = require('../config/keys');
 
 const CoordinatorSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  tokens: [{
-    token: {
+  user: {
+    id: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    name: {
       type: String,
       default: ''
     }
-  }]
-});
-
-CoordinatorSchema.methods.generateAuthToken = function () {
-  const coordinator = this;
-
-  const token = jwt.sign({
-    id: coordinator._id.toHexString()
-  }, keys.jwt.secret, {
-    expiresIn: '12h'
-  }).toString();
-
-  coordinator.tokens = coordinator.tokens.concat([{token}]);
-
-  return coordinator.save().then(() => token);
-};
-
-CoordinatorSchema.statics.findByToken = function (token) {
-  const Coordinator = this;
-
-  let decodedCoordinator = null;
-
-  try {
-    decodeCoordinator = jwt.verify(token, keys.jwt.secret);
-  } catch (e) {
-    return Promise.reject();
   }
-
-  return Coordinator.findOne({
-    '_id': decodedCoordinator.id,
-    'tokens.token': token
-  });
-};
-
-
-CoordinatorSchema.statics.findOrCreate = function (coordinator, cb) {
-  const Coordinator = this;
-
-  return Coordinator.findOne({
-    googleId: coordinator.googleId
-  })
-  .then(savedCoordinator => {
-    if (!savedCoordinator) {
-      const newCoordinator = new Coordinator(coordinator);
-      newCoordinator
-      .save()
-      .then(coordinator => cb(null, coordinator))
-      .catch(err => cb(err));
-    } else {
-      cb(null, savedCoordinator);
-    }
-  })
-  .catch(err => cb(err));
-};
+});
 
 const Coordinator = mongoose.model('Coordinator', CoordinatorSchema);
 
--- models/student.model.js
+++ models/student.model.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const jwt = require('jsonwebtoken');
-const keys = require('../config/keys');
 
 const StudentSchema = new Schema({
   user: {
@@ -25,18 +23,6 @@ const StudentSchema = new Schema({
   }]
 });
 
-StudentSchema.methods.generateAuthToken = function () {
-  const student = this;
-
-  const token = jwt.sign({
-    id: student._id.toHexString()
-  }, keys.jwt.secret, {
-    expiresIn: '12h'
-  }).toString();
-
-  return student.save().then(() => token);
-};
-
 StudentSchema.methods.toJSON = function () {
   const student = this;
   return {
@@ -46,43 +32,6 @@ StudentSchema.methods.toJSON = function () {
     disciplines: student.disciplines
   }
 };
-
-StudentSchema.statics.findByToken = function (token) {
-  const Student = this;
-  
-  let decodedStudent = null;
-
-  try {
-    decodedStudent = jwt.verify(token, keys.jwt.secret);
-  } catch (e) {
-    return Promise.reject();
-  }
-
-  return Student.findOne({
-    '_id': decodedStudent.id
-  });
-};
-
-StudentSchema.statics.findOrCreate = function (student, cb) {
-
-  const Student = this;
-
-  return Student.findOne({
-    googleId: student.googleId
-  })
-  .then(savedStudent => {
-    if(!savedStudent){
-      const newStudent = new Student(student);
-      newStudent
-      .save()
-      .then(student => cb(null, student))
-      .catch(err => cb(err));
-    } else {
-      cb(null, savedStudent);
-    }
-  })
-  .catch(err => cb(err));
-};  
 
 const Student = mongoose.model('Student', StudentSchema);
 
